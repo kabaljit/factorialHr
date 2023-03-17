23fe3f8dfe812cc56da05c19826f5f73054602a4
Kabaljit <kabaljit10@gmail.com>
2023-03-17 17:24:45 +0000

    feat(theme): NO-JIRA Added theme tokens

---
+++ src/theme/tokens.ts
@@ -0,0 +1,178 @@
+/**
+ * Standand app palette
+ */
+
+const palette = {
+  white: {
+    /**
+     * #FFFFFF
+     */
+    0: "#FFFFFF",
+  },
+  black: {
+    /**
+     * #000000
+     */
+    0: "#000000",
+  },
+};
+
+/**
+ * Standard size
+ */
+const sizes = {
+  initial: "initial",
+  inherit: "inherit",
+  auto: "auto",
+  /*
+   * Required for certain scenarios such as clamp
+   */
+  none: "0px",
+  0: "0",
+  1: `1px`,
+  2: `2px`,
+  3: `3px`,
+  4: `4px`,
+  5: `5px`,
+  6: `6px`,
+  7: `7px`,
+  8: `8px`,
+  10: `10px`,
+  12: `12px`,
+  14: `14px`,
+  15: `15px`,
+  16: `16px`,
+  18: `18px`,
+  20: `20px`,
+  22: `22px`,
+  24: `24px`,
+  25: `25px`,
+  26: `26px`,
+  28: `28px`,
+  30: `30px`,
+  32: `32px`,
+  34: `34px`,
+  36: `36px`,
+  40: `40px`,
+  44: `44px`,
+  48: `48px`,
+  50: `50px`,
+  52: `52px`,
+  54: `54px`,
+  56: `56px`,
+  58: `58px`,
+  60: `60px`,
+  64: `64px`,
+  68: `68px`,
+  72: `72px`,
+  76: `76px`,
+  80: `80px`,
+  86: `86px`,
+  88: `88px`,
+  92: `92px`,
+  100: `100px`,
+  110: `110px`,
+  124: `124px`,
+  148: `148px`,
+  163: `163px`,
+  180: `180px`,
+  /**
+   * Standard button width
+   */
+  220: `220px`,
+  280: `280px`,
+  /**
+   * Standard height for Dropdown containers
+   */
+  300: `300px`,
+  /**
+   * Standard input width
+   */
+  444: `444px`,
+  /**
+   * Dropdown input width (due to the close button)
+   */
+  488: `488px`,
+  /**
+   * 100%
+   */
+  full: "100%",
+  /**
+   *75%
+   */
+  threeQuarter: "75%",
+  /**
+   * 50%
+   */
+  half: "50%",
+  /**
+   * 25%
+   */
+  quarter: "25%",
+  /**
+   * 3%
+   */
+  thirds: "33%",
+  /**
+   * 224px
+   */
+  "3xs": `224px`,
+  /**
+   * 256px
+   */
+  "2xs": `256px`,
+  /**
+   * 320px
+   */
+  xs: `320px`,
+  /**
+   * 384px
+   */
+  sm: `384px`,
+  /**
+   * 444px
+   */
+  "1md": `444px`,
+  /**
+   * 448px
+   */
+  md: `448px`,
+  /**
+   * 512px
+   */
+  lg: `512px`,
+  /**
+   * 576px
+   */
+  xl: `576px`,
+  /**
+   * 672px
+   */
+  "2xl": `672px`,
+  /**
+   * 768px
+   */
+  "3xl": `768px`,
+  /**
+   * 896px
+   */
+  "4xl": `896px`,
+  /**
+   * 1024px
+   */
+  "5xl": `1024px`,
+  /**
+   * 1152px
+   */
+  "6xl": `1152px`,
+  /**
+   * 1440px
+   */
+  "7xl": `1440px`,
+  /**
+   * Standard size for tabs
+   */
+  tab: `167px`,
+};
+
+export const tokens = { palette, sizes };
