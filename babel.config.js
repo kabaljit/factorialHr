3109e543cad7fdbe1bc24dfd21205bd59af107f5
Kabaljit <kabaljit10@gmail.com>
2023-03-17 15:44:09 +0000

    feat(config): NO-JIRA setup alias for domain & lib

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,30 @@
+
+const tsconfig = require('./tsconfig.json');
+
+const rawAlias = tsconfig.compilerOptions.paths;
+const alias = {};
+if (rawAlias) {
+  for (const x in rawAlias) {
+    alias[x.replace('/*', '')] = rawAlias[x].map((p) => p.replace('/*', ''));
+  }
+}
+
 module.exports = function (api) {
+
+  
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
     plugins: [
       require.resolve("expo-router/babel"),
+       [
+        'module-resolver',
+        {
+          root: ['./'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias,
+        },
+      ],
     ],
   };
 };
