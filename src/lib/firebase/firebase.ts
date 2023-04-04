6e7ddd991da71db737bc26ac0815c4103ebd2a48
Kabaljit <kabaljit10@gmail.com>
2023-04-04 13:59:30 +0100

    fix(firebase): NO-JIRA Fixed auth no persistence the old state on refresh

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -1,9 +1,13 @@
-import { getAuth } from "firebase/auth";
+import { Auth, getAuth, initializeAuth } from "firebase/auth";
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/storage";
-import { getFirestore } from "firebase/firestore";
+import { Firestore, getFirestore } from "firebase/firestore";
+
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { getReactNativePersistence } from "firebase/auth/react-native";
+import { getApps, getApp, FirebaseApp } from "firebase/app/";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB0yqpmP0r-AXjBE0jmD2inV9BWDeaJDrg",
@@ -15,8 +19,21 @@ const firebaseConfig = {
   measurementId: "G-2WQMKFQZGL",
 };
 
-const app = firebase.initializeApp(firebaseConfig);
+let app: FirebaseApp;
+let auth: Auth;
+let firestore: Firestore;
 
-export const auth = getAuth(app);
-export const firestore = getFirestore(app);
-export { firebase };
+if (getApps().length === 0) {
+  app = firebase.initializeApp(firebaseConfig);
+  auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(AsyncStorage),
+  });
+
+  firestore = getFirestore(app);
+} else {
+  app = getApp();
+  auth = getAuth(app);
+  firestore = getFirestore(app);
+}
+
+export { firebase, app, auth, firestore };
